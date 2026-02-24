81c7245f9ed6da8985cab8336ae0aedecc8a146f
gastonfalena <gastonfalena@gmail.com>
2026-02-24 19:20:13 -0300

    fix: replace none for lax in samteSite

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -8,14 +8,14 @@ const setCookies = (
 ) => {
   res.cookie("accessToken", accessToken, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "lax",
+    secure: true,
+    sameSite: "none",
     maxAge: 15 * 60 * 1000,
   });
   res.cookie("refreshToken", refreshToken, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "lax",
+    secure: true,
+    sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 };
